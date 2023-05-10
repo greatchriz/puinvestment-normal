5ddc042ed5b568d89ab4433b673eb8ee8e6df746
greatchriz <89561544+greatchriz@users.noreply.github.com>
2023-05-10 16:14:34 +0100

    dd

--- assets/js/app.js
+++ assets/js/app.js
@@ -30,7 +30,7 @@
 window.addEventListener('load', fn, false)
 
 function togglePassword() {
-    var x = document.getElementById("myInput");
+    var x = document.getElementById("LoginPassword");
     if (x.type === "password") {
       x.type = "text";
     } else {
